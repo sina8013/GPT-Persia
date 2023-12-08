8ee5369544dfb436d5d20356ab7b4815c1765537
Sina Saeedi <sina8013@gmail.com>
2023-12-08 23:35:12 +0330

    bypass code section

--- content.js
+++ content.js
@@ -6,6 +6,10 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     elements.forEach((element) => {
       element.style.direction = "rtl";
     });
+    const elements2 = document.querySelectorAll("pre");
+    elements2.forEach((element) => {
+      element.style.direction = "ltr";
+    });
   } else if (request.action === "resetRTL") {
     const elements = document.querySelectorAll(
       "main.relative.h-full.w-full.flex-1.overflow-auto.transition-width"
